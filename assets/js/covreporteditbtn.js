f0b5ae0e6d89f0426a7272238f77f2d4488f9549
mekukun <71602023+mekukun@users.noreply.github.com>
2022-04-23 19:08:21 +0800

    Add modal and edit button

---
+++ assets/js/covreporteditbtn.js
@@ -0,0 +1,8 @@
+function covreporteditbtn() {
+  document.getElementById("reportvalidation").removeAttribute("disabled");
+  document
+    .getElementById("callresidentactivitycheck")
+    .removeAttribute("disabled");
+  document.getElementById("roominspectioncheck").removeAttribute("disabled");
+  document.getElementById("reportnote").removeAttribute("disabled");
+}
