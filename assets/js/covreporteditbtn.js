b5d0b5ca6c16ebb6efc1640f7dec7de3b79b998d
mekukun <71602023+mekukun@users.noreply.github.com>
2022-04-25 02:07:36 +0800

    Add functional delete button

--- assets/js/covreporteditbtn.js
+++ assets/js/covreporteditbtn.js
@@ -7,3 +7,8 @@ function covreporteditbtn() {
   document.getElementById("roominspectioncheck").removeAttribute("disabled");
   document.getElementById("reportnote").removeAttribute("disabled");
 }
+
+function deleteRow(r) {
+  var i = r.parentNode.parentNode.rowIndex;
+  document.getElementById("reporttable").deleteRow(i - 1);
+}
