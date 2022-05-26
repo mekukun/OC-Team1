166edae81c8cdab2a65b851d6d34b76cf7059032
MAJD ABOU HASHISH <104038767+MAJDABOUHASHISH@users.noreply.github.com>
2022-05-26 15:41:24 +0300

    added php folder in the assets folder and inside it added config file to connect with the database (#1)

---
+++ assets/php/config.php
@@ -0,0 +1,11 @@
+<?php
+$user='team1'; //the user you have created
+$pass='team1'; //the user password you have created
+$db='community'; //the name of the database you want to connect with
+
+$connection= mysqli_connect('localhost',$user,$pass,$db) or die("Unable to connect"); //connect with the database
+#die(): it is like to catch if something wrong happpens with the connection and stop the code
+
+#you can call this file in other php files, by adding to the other file: include_once("config.php");
+    #then you can use the $connnection variable to connect to the database easily.
+?>
