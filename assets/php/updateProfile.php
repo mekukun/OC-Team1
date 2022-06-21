cdb348ab6588afa66491804345e14660385af042
amirahadanan <76155023+amirahadanan@users.noreply.github.com>
2022-06-21 09:24:15 +0800

    Change database column

--- assets/php/updateProfile.php
+++ assets/php/updateProfile.php
@@ -8,15 +8,14 @@
 	$tel_number = $_POST["tel_number"];
 	$unit_no = $_POST["unit_no"];
 	$gender = $_POST["gender"];
-	$address =  $_POST["address"];
-	$state =  $_POST["state"];
+	$level =  $_POST["level"];
+	$block =  $_POST["block"];
 	$photo = $_POST["photo"];
 
 
-	$stmt = $connection->prepare("UPDATE Resident SET email=?,password=?,name=?,tel_number=?,unit_no=?,gender=?, address=?,state=?, photo=? WHERE resident_id=?");
-	$stmt->bind_param("sssssssssi", $email, $password, $name,$tel_number, $unit_no, $gender, $address, $state, $photo, $_SESSION["userid"]);
+	$stmt = $connection->prepare("UPDATE Resident SET email=?,password=?,name=?,tel_number=?,unit_no=?,gender=?, level=?,block=?, photo=? WHERE resident_id=?");
+	$stmt->bind_param("ssssssissi", $email, $password, $name,$tel_number, $unit_no, $gender, $level, $block, $photo, $_SESSION["userid"]);
 	$stmt->execute();
-
 
 	header("Location: ../../resident/profile.php?action=profile_updated");
 	
